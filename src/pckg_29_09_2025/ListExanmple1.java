f5c198ce43e6de2d9b9a6bf738ee25aaae1f57e2
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-29 14:15:06 +0530

    Commit_List_1

---
+++ src/pckg_29_09_2025/ListExanmple1.java
@@ -0,0 +1,25 @@
+package pckg_29_09_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExanmple1 {
+	
+	
+	public static void main(String[] args) {
+		
+		List<String> fruits=new ArrayList<>();
+		
+		fruits.add("Banana");
+		fruits.add("Mango");
+		fruits.add("Pineapple");
+		
+		System.out.println(fruits);
+		
+	     while ("fruits" != "Banana") {
+			
+		}
+		
+	}
+
+}
