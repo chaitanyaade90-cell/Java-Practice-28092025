bad96cbe6c2ae1757258d65320d2685fa5ce3ea6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-30 07:57:02 +0530

    IfElse_Commit

---
+++ src/If_else_30_09_2025/Ifelse_Class_1.java
@@ -0,0 +1,23 @@
+package If_else_30_09_2025;
+
+public class Ifelse_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=25;
+		boolean citizen=true;
+		
+		if(age>15);
+		
+		if (citizen) {
+			System.out.println("Students are Eligible for vote");
+			
+		} else {
+			System.out.println("Students are not Eligible for Vote");
+
+		}
+
+	}
+
+}
---
+++ src/If_else_30_09_2025/Ifelse_Class_2.java
@@ -0,0 +1,24 @@
+package If_else_30_09_2025;
+
+public class Ifelse_Class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int id=30;
+		boolean citizen=true;
+		
+		if(id>20);
+		
+		if (citizen) {
+			
+			System.out.println("Company are important");
+			
+		} else {
+			System.out.println("Company are not imprtant");
+
+		}
+
+	}
+
+}
