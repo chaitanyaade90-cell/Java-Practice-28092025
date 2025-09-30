56deb0a5a4553f20e123a2b63ef3ddfe1840d9de
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-30 08:20:36 +0530

    Method_1

---
+++ src/MethodA_Arguments_pckg/Method_Arguments_Class.java
@@ -0,0 +1,38 @@
+package MethodA_Arguments_pckg;
+
+public class Method_Arguments_Class {
+	
+	public static void add(int a1, int a2) {
+		int sum= a1+a2;
+		System.out.println(sum);
+	}
+	
+	public static void sub(int a3, int a4) {
+		int sum=a3-a4;
+		System.out.println(sum);
+	}
+	
+	public static void mul(int a5, int a6) {
+		int sum=a5*a6;
+		System.out.println(sum);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Method_Arguments_Class.add(200, 300);
+		Method_Arguments_Class.sub(300, 100);
+		Method_Arguments_Class.mul(200, 100);
+		
+		
+		
+		
+			
+		
+		
+
+	}
+
+}
