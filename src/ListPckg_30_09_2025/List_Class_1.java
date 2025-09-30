f8b9434c879b186e03d62dec8b86bacd356ffcc6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-30 07:37:17 +0530

    List_1 & 2_30_09

---
+++ src/ListPckg_30_09_2025/List_Class_1.java
@@ -0,0 +1,27 @@
+package ListPckg_30_09_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Cars=new ArrayList<>();
+		
+		Cars.add("Toyota");
+		Cars.add("Xuv");
+		Cars.add("MG hector");
+		
+		System.out.println("Cars :" + Cars);
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("All cars Are Important");
+			
+		}
+
+	}
+
+}
---
+++ src/ListPckg_30_09_2025/List_Class_2.java
@@ -0,0 +1,28 @@
+package ListPckg_30_09_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_Class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Companys=new ArrayList<>();
+		
+		Companys.add("Lenovo");
+		Companys.add("Dell");
+		Companys.add("HMD");
+		
+		System.out.println("Companys: " + Companys);
+		
+		for (int i = 0; i < 10; i++) {
+			
+			System.out.println("All are Laptop Companys");
+			
+		}
+		
+
+	}
+
+}
