6e66bdbf1f7f52d37454cf0ac7d577f5f0c69cb7
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-30 16:34:07 +0530

    HashList_1

---
+++ src/HasList_30_09_2025pckg/HashList_Class_1.java
@@ -0,0 +1,32 @@
+package HasList_30_09_2025pckg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashList_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Class=new ArrayList<>();
+		
+		Class.add("Java");
+		Class.add("Python");
+		Class.add("Eclipse");
+		
+		System.out.println(Class);
+		
+		for (int i = 0; i < Class.size(); i++) {
+			
+		}
+		for (int i = 0; i < Class.lastIndexOf(args); i++) {
+			
+			System.out.println(Class);
+			
+		}
+		
+		
+
+	}
+
+}
