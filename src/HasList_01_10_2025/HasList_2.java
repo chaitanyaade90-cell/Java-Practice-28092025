8ec11562b4d726eb279499a29691a18772f51f1c
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-01 08:16:16 +0530

    Hash_List_2

---
+++ src/HasList_01_10_2025/HasList_2.java
@@ -0,0 +1,27 @@
+package HasList_01_10_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HasList_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Company=new ArrayList<>();
+		
+		Company.add("Tata");
+		Company.add("Excelr");
+		Company.add("Mahindra");
+		
+		for (int i = 0; i < Company.size(); i++) {
+			
+			System.out.println(Company);
+			
+		}
+		
+	
+
+	}
+
+}
