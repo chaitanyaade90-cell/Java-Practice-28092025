8a1c9fce1d6769d728f0193329698bcaede76a39
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-02 10:58:51 +0530

    Class_3

---
+++ src/List_02_10_2025/ListClass_3.java
@@ -0,0 +1,25 @@
+package List_02_10_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Books=new ArrayList<>();
+		
+		Books.add("The Success");
+		Books.add("Nexos");
+		Books.add("The Secrt");
+		
+		for (int i = 0; i < Books.size(); i++) {
+			
+			System.out.println(Books);
+			
+		}
+
+	}
+
+}
