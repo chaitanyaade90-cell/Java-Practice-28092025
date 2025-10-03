71b68b2499f2951f3065bccdb2f4a0371579bac6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-03 22:44:08 +0530

    IfLoop_1

---
+++ src/Pckg_03_10_2025/IfLoop_1.java
@@ -0,0 +1,28 @@
+package Pckg_03_10_2025;
+
+public class IfLoop_1 {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=20;
+		
+		boolean citizen=true;
+		
+		if(age>=15);
+		
+		if (citizen) {
+			
+			System.out.println("All students are passed exam");
+			
+		} else {
+			
+			System.out.println("All students are failed");
+
+		}
+
+	}
+
+}
