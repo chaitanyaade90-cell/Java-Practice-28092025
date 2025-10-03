c53d4d321b5731fcace6eb821ef16dbdca7453e5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-03 22:27:14 +0530

    Method_Test1

---
+++ src/Pckg_03_10_2025/MethodA_2.java
@@ -0,0 +1,37 @@
+package Pckg_03_10_2025;
+
+public class MethodA_2 {
+	
+	public static void add(int a1, int a2) {
+		
+		int sum=a1+a2;
+		
+		System.out.println(sum);
+	}
+	
+	public static void sub(int a3, int a4) {
+		
+		int sum=a3-a4;
+		
+		System.out.println(sum);
+	}
+	
+	public static void mul(int a5, int a6) {
+		
+		int sum=a5*a6;
+		
+		System.out.println(sum);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodA_2.add(3000, 2000);
+		MethodA_2.sub(5000, 4000);
+		MethodA_2.mul(1000, 500);
+		
+
+	}
+
+}
