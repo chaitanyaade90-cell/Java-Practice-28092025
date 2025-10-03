cb9552f4adec313b849deec09dc33f4477822051
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-03 07:54:15 +0530

    Method_03_10_25_1

---
+++ src/Pckg_03_10_2025/MethodArrugument_1.java
@@ -0,0 +1,38 @@
+package Pckg_03_10_2025;
+
+public class MethodArrugument_1 {
+	
+	public static void add( int a1, int a2) {
+		int sum=a1+a2;
+		
+		System.out.println(sum);
+		
+	}
+	
+	public static void sub(int a3, int a4) {
+		int sum=a3-a4;
+		System.out.println(sum);
+	}
+	
+	public static void mul(int a5, int a6) {
+		int sum=a5*a6;
+		
+		System.out.println(sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodArrugument_1.add(500, 600);
+		MethodArrugument_1.sub(700, 200);
+		MethodArrugument_1.mul(400, 300);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
