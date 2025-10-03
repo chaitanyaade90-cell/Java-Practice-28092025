1651c7cb43b75636797122488ef6322389248352
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-03 23:47:42 +0530

    List_01

---
+++ src/List_Class_1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Cars=new ArrayList<>();
+		
+		Cars.add("Tarzan");
+		Cars.add("Tavera");
+		Cars.add("Carens");
+		
+		for (int i = 0; i < Cars.size(); i++) {
+			
+			System.out.println(Cars);
+			
+			
+			
+		}
+		
+
+	}
+
+}
