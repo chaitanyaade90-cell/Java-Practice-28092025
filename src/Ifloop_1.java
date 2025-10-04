3165f16b82b9592b417f3ec20fecea6adbf0ce06
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-04 15:36:46 +0530

    Forloop_1

---
+++ src/Ifloop_1.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ifloop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 100; i++) {
+			
+			System.out.println(i);
+			
+			List<String>Cars=new ArrayList<>();
+			
+			Cars.add("Moto");
+			Cars.add("Bugati");
+			Cars.add("Nope");
+			
+			System.out.println(Cars);
+		}
+
+	}
+
+}
