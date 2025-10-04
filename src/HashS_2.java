8cc195dc45a3d501013752dadeebbd49c5c34c2e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-04 23:23:36 +0530

    List2

---
+++ src/HashS_2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class HashS_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Cars=new ArrayList<>();
+		
+		Cars.add("Bugati ");
+		Cars.add("Audi A");
+		Cars.add("Test");
+		Cars.add("Benz");
+		
+		for (int i = 0; i < Cars.size(); i++) {
+			
+			if(Cars.get(i).contains("Audi"));
+			
+			System.out.println(Cars.get(i));
+			
+		}
+		
+
+	}
+
+}
