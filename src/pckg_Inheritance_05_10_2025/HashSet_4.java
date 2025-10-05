5328221bd28cc85d74578e7bc0082ae6f6d5b7ab
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-05 16:58:57 +0530

    Set4

--- src/pckg_Inheritance_05_10_2025/HashSet_4.java
+++ src/pckg_Inheritance_05_10_2025/HashSet_4.java
@@ -1,25 +1,22 @@
 package pckg_Inheritance_05_10_2025;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Set_2 {
+public class HashSet_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Set<Integer> numbers=new HashSet<>();
 		
-		numbers.add(1000);
-		numbers.add(5000);
-		numbers.add(10000);
+		numbers.add(9000);
+		numbers.add(2000);
+		numbers.add(3000);
 		
-		for(int num : numbers);
+		for(int num: numbers);
 		
 		System.out.println(numbers);
-		
-		
 
 	}
 
