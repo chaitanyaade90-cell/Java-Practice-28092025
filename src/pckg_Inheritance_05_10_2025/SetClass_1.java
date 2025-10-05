6f6da29b6ddd9ede70921e6450b47f8452115916
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-05 15:34:35 +0530

    Set_1 & 2

---
+++ src/pckg_Inheritance_05_10_2025/SetClass_1.java
@@ -0,0 +1,23 @@
+package pckg_Inheritance_05_10_2025;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetClass_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<Integer> numbers=new HashSet<>();
+		
+		numbers.add(100);
+		numbers.add(200);
+		numbers.add(300);
+		
+		for(int num : numbers);
+		
+		System.out.println(numbers);
+
+	}
+
+}
---
+++ src/pckg_Inheritance_05_10_2025/Set_2.java
@@ -0,0 +1,24 @@
+package pckg_Inheritance_05_10_2025;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<Integer> numbers=new HashSet<>();
+		
+		numbers.add(1000);
+		numbers.add(5000);
+		numbers.add(10000);
+		
+		for(int num : numbers);
+		
+		System.out.println(numbers);
+
+	}
+
+}
