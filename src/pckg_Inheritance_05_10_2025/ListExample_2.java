fe1bb9485d109c656e78d167408492d8fbdb1aec
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-05 14:30:11 +0530

    List_2

---
+++ src/pckg_Inheritance_05_10_2025/ListExample_2.java
@@ -0,0 +1,24 @@
+package pckg_Inheritance_05_10_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String>Company=new ArrayList<>();
+		
+		Company.add("Vi");
+		Company.add("Airtel");
+		Company.add("Bsnl");
+		
+		for(String Company1: Company) {
+			System.out.println("Company : " + Company);
+			
+		}
+
+	}
+
+}
