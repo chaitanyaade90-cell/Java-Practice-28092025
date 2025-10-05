721c0fe0a99e4694a74a63e425d7c54348fae8d5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-05 11:35:52 +0530

    Inherit_1

---
+++ src/pckg_Inheritance_05_10_2025/Inheritance_1.java
@@ -0,0 +1,45 @@
+package pckg_Inheritance_05_10_2025;
+
+
+class A{
+
+	int a=10;
+
+	void inherit() {
+
+		System.out.println(a);
+	}
+
+
+}
+
+class B 
+{
+	int b=20;
+
+	void inherit2() {
+
+		System.out.println(b);
+	}
+}
+
+public class Inheritance_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//A obA=new A();
+		//obA.inherit();
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
