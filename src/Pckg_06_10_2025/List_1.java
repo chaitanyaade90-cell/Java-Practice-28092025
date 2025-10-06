2741a832edfe936a828d66e8bcb8624747ab36a7
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-06 08:01:03 +0530

    Hash_1

---
+++ src/Pckg_06_10_2025/List_1.java
@@ -0,0 +1,29 @@
+package Pckg_06_10_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_1 {
+
+	private static int j;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> Company=new ArrayList<>();
+		
+		Company.add("Jio");
+		Company.add("chroma");
+		Company.add("QuickHill");
+		
+		for (int i = 0; i < Company.size(); i++) {
+			
+			if(Company.get(j) != "jio");
+			
+			System.out.println(Company);
+			
+		}
+
+	}
+
+}
